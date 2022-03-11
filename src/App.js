0bccfdcf175f06cc7c4b0e5033bed94ae46c660e
Muhammad Faizan <66197447+Muhammadfaizanjanjua109@users.noreply.github.com>
2022-03-11 22:08:58 +0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cardan from './Cardan';
 function App() {
   return (
     <div className="App">
-      <h1 className='h1'>Day 6 of 100 Days Code Chellenge</h1>
+      <h1 className='h1'>Day 7 of 100 Days Code Chellenge</h1>
       <p>All Data by fetch api and design using bootstrap for react</p>
     <Api/>
    
