03212d028f6a1f44ebb917cdf9b0a34799b26109
Muhammad Faizan <66197447+Muhammadfaizanjanjua109@users.noreply.github.com>
2022-03-11 22:24:36 +0500

    3 Navbars added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Api from './Api';
 import Cardan from './Cardan';
+import Mainnav from './MainNav';
+
 
 function App() {
   return (
     <div className="App">
+    <Mainnav/>
       <h1 className='h1'>Day 7 of 100 Days Code Chellenge</h1>
       <p>All Data by fetch api and design using bootstrap for react</p>
     <Api/>
